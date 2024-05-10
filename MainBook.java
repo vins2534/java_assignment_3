bd610e8ae3af7f4210faff01292ea0e6257a95db
vins2534 <129215003+vins2534@users.noreply.github.com>
2024-05-10 19:49:47 +0530

    Create MainBook.java

---
+++ MainBook.java
@@ -0,0 +1,20 @@
+package assignment3_books;
+
+public class MainBook 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		BookFunctions b = new BookFunctions();
+		b.addBook();
+		b.printBookToString();
+		/*
+		b.printBooksDetails();
+		b.deleteBook();
+		b.printBooksDetails();
+		*/
+		
+	}
+
+}
